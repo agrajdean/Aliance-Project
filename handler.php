41403c81e9152f458cfacbaa0cf3c84877166c10
agrajdean <agrajdean@mail.ru>
2024-03-18 19:26:49 +0200

    Added: Telegram bot created

--- handler.php
+++ handler.php
@@ -1,5 +1,27 @@
 <?php
-$user_name = $_POST["username"];
-$user_phone = $_POST["userphone"];
+$user_name = htmlspecialchars($_POST["username"]);
+$user_phone = htmlspecialchars($_POST["userphone"]);
+
+$token = "7042152134:AAF1VGuaB84tfvTzObq3MuEHriw21dg4ch0";
+$chat_id = "5297648239";
+
+$formData = array(
+  "Клиент: " => $user_name,
+  "Телефон: " => $user_phone
+);
+
+foreach($formData as $key => $value) {
+  $text .= $key . "<b>" . urlencode($value) . "</b>" . "%0A" ;
+}
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text={$text}&parse_mode=html", "r");
+
+if ($sendToTelegram) {
+  echo "Success";
+} else {
+  echo "Error";
+}
+
+
 echo "Привет, <b>" . $user_name . "</b>" . "<br>";
 echo "Ваш номер телефона: <b>" . $user_phone . "</b>";
